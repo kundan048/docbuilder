e5eeb55e20a9755bfa19e252c457cba8b5df7b34
Kundan <kundan04826@gmail.com>
2018-08-28 22:11:03 +0530

    login and registration page

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ app.get("/", function(req, res){
    
 });
 
+app.get("/login", function(req, res){
+	res.render("login",{
+		title: "Login"
+	});
+});
+
+app.get("/register", function(req, res){
+	res.render("register",{
+		title: "Register"
+	});
+});
+
 
 
 app.listen(process.env.PORT || 3000, function(){
