398b2b80efaadd252fb05f78337f8ad766ae72f2
Kundan <kundan04826@gmail.com>
2018-09-27 12:35:23 +0530

    download button added

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ var express			 		= require('express'),
 	LocalStrategy	 		= require('passport-local'),
 	passportLocalMongoose	= require('passport-local-mongoose'),
 	expressSession 			= require('express-session'),
-	User					= require('./models/user');
+	User					= require('./models/user'),
+	officeClippy 			= require('office-clippy'),
+	docx 					= officeClippy.docx,
+	exporter 				= officeClippy.exporter,
+	fs 						= require('fs');
 	require("dotenv/config");
+
 	
 
-mongoose.connect(process.env.CODE);
-//mongoose.connect("mongodb://localhost/Document_help", { useNewUrlParser: true });
+//mongoose.connect(process.env.CODE);
+mongoose.connect("mongodb://localhost/Document_help", { useNewUrlParser: true });
 
 var app = express();
 app.set("view engine", "ejs");
@@ -113,7 +118,33 @@ app.get("/logout", function(req, res){
 });
 
 app.get("/homepage", isLoggedIn, function(req, res){
-	res.render("homepage.ejs")
+	res.render("homepage")
+});
+
+app.get("/application", isLoggedIn, function(req, res){
+	res.render("application");
+});
+
+app.post("/application", isLoggedIn, function(req, res){
+	
+})
+
+app.get('/download', isLoggedIn, function(req, res){
+	var doc = docx.create();
+	var to = docx.createText("To,").break();
+	var thePrincipal = docx.createText("The Principal,").break();
+	var schoolName = docx.createText(req.query.school_name + ",").break();
+	var Address = docx.createText(req.query.address + ",").break();
+	var date = docx.createText(req.query.date + ",").break();
+	var greeting = docx.createText("Sir/Ma'am',").break();
+	var paragraph = docx.createText("With due respect I beg to state that I am not in a position to attend the school as I am down with "+ req.query.reason +". Since it is a communicable disease, I have been advised quarantine and a few days complete rest. Therefore kindly grant me leave for ten days").break();
+	var thanku = docx.createText("Thanking you,").break();
+	var yoursobe = docx.createText("Yours obediently,").break();
+	var name = docx.createText(req.query.name).break();
+	var para2 = docx.createParagraph().addText(to).left().addText(thePrincipal).left().addText(schoolName).left().addText(Address).left().addText(date).left().addText(greeting).left().addText(paragraph).left().addText(thanku).left().addText(yoursobe).left().addText(name).left();
+	doc.addParagraph(para2);
+	exporter.express(res, doc, 'example');
+
 });
 
 // Listening to the server
