36a0c012af42a30304d00f5a98bcdf9132b88414
Anupam Kumar <36506993+anupam1109@users.noreply.github.com>
2019-05-20 00:01:07 +0530

    Add recommendation letter

---
+++ models/letter_2.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var letter_2Schema = mongoose.Schema({
+	name					: String,
+	designation 			: String,
+	address 				: String,
+	date 					: String,
+	college_name            : String,
+	
+	student_name			: String,
+	applied_college_name    : String,
+	duration				: String,
+	email					: String,
+	committed 				: {
+	    type: Date,
+	    default: Date.now
+	}
+});
+
+module.exports = mongoose.model("Letter_2", letter_2Schema);
---
+++ routes/letter/recentletter_2.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+
+// Load Model
+var User = require('../../models/user');
+var Letter_2 = require('../../models/letter_2');
+
+// Load Helpers
+var {isLoggedIn} = require('../../helpers/auth');
+
+router.get("/recentletter_2", isLoggedIn, function (req, res) {
+    User.findById(req.user._id).populate("letter_2").exec(function (err, foundUser) {
+        if (err) {
+            console.log(err);
+            // req.flash("error", "Something went wrong! Please try again!");
+            res.redirect("/letter_2");
+        } else {
+            //console.log(foundUser);
+            if (foundUser.letter_2.length == 0) {
+
+                res.redirect("/letter_2");
+            } else {
+                res.render("recentletter_2", {user: foundUser, moment: moment, myid: 0});
+            }
+        }
+    });
+});
+
+router.get("/recentletter_2/:id", isLoggedIn, function (req, res) {
+    User.findById(req.user._id).populate("letter_2").exec(function (err, foundUser) {
+        if (err) {
+            res.redirect("/letter_2");
+        } else {
+            var item = foundUser.letter_t1_t1.find(item => item.id === req.params.id);
+            //console.log(item);
+            //console.log(foundUser);
+            res.render("recentletter_2", {user: foundUser, moment: moment, myid: item});
+        }
+    });
+});
+
+module.exports = router;
