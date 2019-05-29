c51482ea37c859de6db6ba5ea3c75ec7aa03cbeb
Anupam Kumar <36506993+anupam1109@users.noreply.github.com>
2019-05-29 23:59:18 +0530

    Add Transfer application template

--- routes/application/download_4.js
+++ routes/application/download_4.js
@@ -26,19 +26,19 @@ router.get('/download_4/:id', isLoggedIn, function (req, res) {
 
             var doc = docx.create();
             var to = docx.createText("To");
-            var manager_name = docx.createText(downUser.manager_name + ",").break().break();
-            var manager = docx.createText("Regional Manager,").break();
+            var supervisor_name = docx.createText(downUser.supervisor_name).break();
+            var supervisor_designation = docx.createText(downUser.supervisor_designation).break();
             var company_name = docx.createText(downUser.company_name).break();
-            var subject = docx.createText("Subjetc : " + downUser.subject + ".").break().break();
             var date = docx.createText(downUser.date).break().break();
-            var content1 = docx.createText("This is to inform you that my wife is in her final stages of pregnancy and is due to deliver by the end of this month.").break().break();
-            var content2 = docx.createText("She has been facing some minor complications and has been unwell for the past few days. As we do not have parental support at this time, I need to stay with her until she delivers, and for the next few weeks after delivery.").break().break();
-            var content3 = docx.createText("I, therefore, request you for paternity leave for a period of " + downUser.no_of_weeks + " weeks, with effect from " + downUser.start_date + " to " + downUser.end_date + ". I hope that this request will be sanctioned.").break().break();
-            var content4 = docx.createText("As I will be away for a considerably long period, I have entrusted " + downUser.colleague_name + " with my responsibilities and duties. In the case of an unmanageable emergency, please contact me at " + downUser.email +  ".").break().break();
-            var regards = docx.createText("sincerely,").break().break();
+            var subject = docx.createText("Subject: Transfer Application.").break().break();
+            var salutation = docx.createText("Sir/Ma'am',").break().break();
+            var content1 = docx.createText("I," + downUser.name + " , am working for your company, " + downUser.company_name + ", as a " + downUser.designation + ". With this letter, I wish to convey my request to you for a transfer at our " + downUser.branch +  "branch. This is because " + downUser.reason + ".").break();
+            var content2 = docx.createText("I have made quite a few good memories and befriended all of my colleges the current office branch. I will miss them dearly. I can’t help the circumstance. I have no other option but to work at the " + downUser.branch + ". I solemnly swear to show the same performance as I have demonstrated here. I wish you will consider my request for this transfer and do the needful. In anticipation of a favorable reply.").break().break();
+            
+            var regards = docx.createText("Sincerely,").break().break();
             var name = docx.createText(downUser.name).break();
 
-            var para1 = docx.createParagraph().addText(to).left().addText(manager_name).left().addText(manager).left().addText(company_name).left().addText(date).left().addText(subject).left().addText(content1).left().addText(content2).left().addText(content3).left().addText(content4).left().addText(regards).left().addText(name).left();
+            var para1 = docx.createParagraph().addText(to).left().addText(supervisor_name).left().addText(supervisor_designation).left().addText(company_name).left().addText(date).left().addText(subject).left().addText(salutation).left().addText(content1).left().addText(content2).left().addText(regards).left().addText(name).left();
             doc.addParagraph(para1);
             var docname = downUser.name + "_application";
             exporter.express(res, doc, docname);
